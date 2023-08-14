33ed7edea7b226732cb8567a34e6a4e5fddfedc7
Trung Nguyen <trung.nguyen@codelink.io>
2023-08-15 00:09:20 +0700

    feat: update type of product

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,4 +1,4 @@
 import type { RouterOutputs } from ".";
 
-export type ProductList = RouterOutputs["product"]["productList"];
+export type ProductList = RouterOutputs["product"]["productList"]['data'];
 export type Product = ProductList[number];
