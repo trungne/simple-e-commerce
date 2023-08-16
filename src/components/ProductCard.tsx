0691572fbc5e66df8c3324cf1838f856fdbdc6b2
Trung Nguyen <trung.nguyen@codelink.io>
2023-08-16 16:32:36 +0700

    feat: add search input

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,41 +1,58 @@
-import { Card, Group,Text,Button,Image, Box } from "@mantine/core";
+import { Card, Group, Text, Button, Image } from "@mantine/core";
 import type { MouseEventHandler } from "react";
 import type { Product } from "~/types/product";
 
 type Props = {
-    product: Product
-    onClick?: MouseEventHandler<HTMLDivElement> | undefined;
-}
+  product: Product;
+  onClick?: MouseEventHandler<HTMLDivElement> | undefined;
+};
 
-export const ProductCard = ({product, onClick}: Props) => {
+export const ProductCard = ({ product, onClick }: Props) => {
   return (
-      <Card
-        style={{ height: 250, flexBasis: "30%" }}
-        shadow="sm"
-        padding="sm"
-        withBorder
+    <Card
+      style={{ height: 250, flexBasis: "30%" }}
+      shadow="sm"
+      padding="sm"
+      withBorder
+    >
+      <Card.Section
         onClick={onClick}
-        sx={() => ({
-          cursor: 'pointer'
-        })}
+        sx={{
+          cursor: "pointer",
+        }}
       >
-        <Card.Section>
-          <Image src={product.image} height={100} alt="Image" />
-        </Card.Section>
+        <Image src={product.image} height={100} alt="Image" />
+      </Card.Section>
 
-        <Group position="apart" mt="md" mb="xs">
-          <Text size="sm" weight={500} truncate>
-            {product.name}
-          </Text>
-        </Group>
-
-        <Text size="xs" color="dimmed" lineClamp={2}>
-          {product.description}
+      <Group
+        position="apart"
+        mt="md"
+        mb="xs"
+        onClick={onClick}
+        sx={{
+          cursor: "pointer",
+        }}
+      >
+        <Text size="sm" weight={500} truncate>
+          {product.name}
         </Text>
+      </Group>
 
-        <Button variant="light" color="blue" fullWidth mt="lg" radius="md">
-          Buy now
-        </Button>
-      </Card>
+      <Text
+        size="xs"
+        color="dimmed"
+        lineClamp={2}
+        onClick={onClick}
+        sx={{
+          cursor: "pointer",
+        }}
+      >
+        {product.description}
+      </Text>
+
+      <Button variant="light" color="blue" fullWidth mt="lg" radius="md">
+        Buy now
+      </Button>
+    </Card>
   );
 };
